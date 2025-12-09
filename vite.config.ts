0cd8b8bc0f37138a6a28f6dcbd89cbc7a6df0df9
yaseenlenceria <124892871+yaseenlenceria@users.noreply.github.com>
2025-12-09 18:14:12 +0500

    feat: Update script imports and Vite config
    Adds a new import map to `index.html` for CDN-hosted dependencies, simplifying setup. Updates `vite.config.ts` to correctly load environment variables for production builds.

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '');
+  const env = loadEnv(mode, (process as any).cwd(), '');
   return {
     plugins: [react()],
     define: {
